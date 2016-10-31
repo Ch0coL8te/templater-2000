8cc0b9940de04e27cd45800c138a725906b03ed1
Stanislav Burov <ch0col8te@me.com>
2016-11-01 02:52:01 +0300

    Huge overhaul

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const companies = require('./storage/companies.json'); 
+
 $(function() {
     $('.main-form').submit(function() {
         $('[data-target]').each(function () {
@@ -7,5 +9,25 @@ $(function() {
         });
         $('.document').toggleClass('hidden');
         return false;
+    });
+    
+    // Companies selector
+    let $companySelect = $('.companySelect');
+    $.each(companies, function (key, value) {
+        let $option = $('<option>')
+        .val(key)
+        .text(value.companyShortName);
+
+        $companySelect.append($option);
+    });
+    
+    $companySelect.change(function () {
+        let $this = $(this);
+        let company = companies[$this.val()];
+        if (company != null) {
+            $.each(company, function (key, value) {
+                $('[data-target="' + key + '"]').val(value);
+            });
+        }
     });
 });
