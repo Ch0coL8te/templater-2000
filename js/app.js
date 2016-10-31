145f34f39e000f6dcbae90360eecdf1545b07a6a
Stanislav Burov <ch0c0l8te@me.com>
2016-10-31 16:06:16 +0300

    Migrating to Bootstrap 4

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(function() {
         $('[data-target]').each(function () {
             var $this = $(this);
             var $target = $('[data-id="' + $this.data('target') + '"]');
-            $target.text($this.val());
+            $target.html($this.val());
         });
         $('.document').toggleClass('hidden');
         return false;
