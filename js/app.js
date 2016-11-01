56d326b56997af62f148cc62aca9460fec0c3872
Stanislav Burov <ch0c0l8te@me.com>
2016-11-01 14:42:21 +0300

    add js comments

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@ const companies = require('./storage/companies.json');
 const {ipcRenderer} = require('electron');
 
 $(function() {
-    let $companySelect = $('.companySelect');
+    let $companySelect = $('.companySelect');  // "saving" a DOM element
 
-    let addCompany = (key, value) => {
+    let addCompany = (key, value) => {         // Creating options for 'select' element
         let $option = $('<option>')
             .val(key)
             .text(value.companyShortName);
@@ -13,36 +13,39 @@ $(function() {
     };
 
     let updateCompanies = () => {
-        $companySelect.empty();
+        $companySelect.empty();                                 
         addCompany('new', {companyShortName: 'Новая компания'});
         $.each(companies, addCompany);
     };
 
     $('.main-form').submit(function() {
-        let companyKey = $companySelect.val();
-        if (companyKey === 'new') {
-            companyKey = Date.now();
-            companies[companyKey] = {};
+        let companyKey = $companySelect.val();  // Searching selected item value (as a key) in "database"
+        if (companyKey === 'new') {             // If key is 'new'
+            companyKey = Date.now();            // then set it to unique 'Date' value (numbers)
+            companies[companyKey] = {};         
         }
         let company = companies[companyKey];
 
-        $('[data-target]').each(function () {
-            let $this = $(this);
+        $('[data-target]').each(function () {            // For each input with 'data-target'
+            let $this = $(this);                         // we are sending a value in span with the same 'data-id' value
             let id = $this.data('target');
-            let $target = $('[data-id="' + id + '"]');
+            let $target = $('[data-id="' + id + '"]');   
             let val = $this.val();
             $target.text(val);
             company[id] = val;
         });
-
-        $('.document').toggleClass('hidden');
-        ipcRenderer.send('storage:update', companies);
-        updateCompanies();
+        
+        let finalDocument = $('.document');
+        finalDocument.show(2000); // Document appear when btn is clicked
+        
+        
+        ipcRenderer.send('storage:update', companies);  // Sending 'sotage:update' event to Main Process
+        updateCompanies();              
 
         return false;
     });
-
-    $companySelect.change(function () {
+                    
+    $companySelect.change(function () {                
         let $this = $(this);
         let company = companies[$this.val()];
         if (company == null) {
